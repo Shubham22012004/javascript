f4cb1e651f2b532f8435d5d0c3376be614438ec0
Shubham22004 <shubhamyadavop2002@gmail.com>
2024-10-19 23:58:55 +0530

    adding button and add css using js

--- js.prac/script.js
+++ js.prac/script.js
@@ -1,6 +1,6 @@
 let btn=document.createElement('button');
-btn.textContent="heool what you want to do";
-console.log(btn);
+btn.textContent="click me";
+btn.style.cssText=`background-Color:red; color:white; font-Size:30px`;
 
 let body = document.querySelector('body');
-body.append(btn);
+body.prepend(btn);
